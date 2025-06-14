b40ad569c549bc5fe68833d8f72886c9e53dafd8
Hoàng Mạnh Toàn <toanhmph44108@fpt.edu.vn>
2025-06-14 16:37:29 +0700

    feat: client shop

--- resources/views/clients/layouts/sidebar.blade.php
+++ resources/views/clients/layouts/sidebar.blade.php
@@ -27,14 +27,14 @@
             <div class="collapse navbar-collapse bg-white" id="navbarCollapse">
                 <div class="navbar-nav mx-auto">
                     <a href="{{ route('home.index') }}" class="nav-item nav-link active">Trang chủ</a>
-                    <a href="{{ route('shop.index') }}" class="nav-item nav-link">Shop</a>
-                    <a href="shop-detail.html" class="nav-item nav-link">Shop Detail</a>
+                    <a href="{{ route('shop.index') }}" class="nav-item nav-link">Cửa hàng</a>
+                    <a href="shop-detail.html" class="nav-item nav-link">Chi tiết cửa hàng</a>
                     <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Trang</a>
                         <div class="dropdown-menu m-0 bg-secondary rounded-0">
                             <a href="cart.html" class="dropdown-item">Giỏ hàng</a>
                             <a href="chackout.html" class="dropdown-item">Thanh toán</a>
-                            <a href="testimonial.html" class="dropdown-item">Testimonial</a>
+                            <a href="testimonial.html" class="dropdown-item">Ưu đãi</a>
                             <a href="{{ route('news.index') }}" class="dropdown-item">Tin tức</a>
                         </div>
                     </div>
