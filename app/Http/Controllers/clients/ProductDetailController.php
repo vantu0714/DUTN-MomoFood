f4ec32d5b6f95ac8e63afbaed8cc76d3e08a6fbd
vantu <tudvph42850@fpt.edu.vn>
2025-06-22 23:52:03 +0700

    fix: fix lai trang shop

--- app/Http/Controllers/clients/ProductDetailController.php
+++ app/Http/Controllers/clients/ProductDetailController.php
@@ -10,14 +10,13 @@ class ProductDetailController extends Controller
     public function show($id)
     {
         $product = Product::with([
-    'category',
-    'comments' => function ($query) {
-        $query->latest(); // sắp xếp theo created_at DESC
-    },
-    'comments.user'
-])->findOrFail($id);
+            'category',
+            'comments' => function ($query) {
+                $query->latest(); // sắp xếp theo created_at DESC
+            },
+            'comments.user'
+        ])->findOrFail($id);
 
         return view('clients.product-detail', compact('product'));
     }
 }
-
