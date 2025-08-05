c2ae94e8f4e1866362521bd54400b4dd84cc21c0
Minhhoa20033 <nguyenminhhoa30903@gmail.com>
2025-08-05 21:05:33 +0700

    fix:tesst ai

--- resources/views/clients/layouts/footer.blade.php
+++ resources/views/clients/layouts/footer.blade.php
@@ -99,12 +99,14 @@
 
  <!-- Copyright End -->
 
+
  <!-- Back to Top -->
  <a href="#" class="btn border-3 border-0 rounded-circle back-to-top text-white"
      style="background-color: #db735b;"><i class="fa fa-arrow-up"></i></a>
 
 
  <!-- JavaScript Libraries -->
+
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
  <script src="{{ asset('clients/lib/easing/easing.min.js') }}"></script>
